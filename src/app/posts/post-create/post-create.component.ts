06f0c8f3bdfef06b80cb2173ef9ec897b5544898
bhuwan <bkspoudel@gmail.com>
2023-08-28 10:40:52 +0200

    create post-create component inside posts/post-create

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl:'./post-create.component.html',
+  styleUrls:['./post-create.component.scss'],
+})
+
+export class PostCreateComponent {
+
+}
